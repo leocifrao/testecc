6cd47074dc6d57ee61947de380270cc1bb9e7c03
leocifrao <leodf1000@gmail.com>
2020-03-20 16:44:41 -0300

    adicionando botoes

--- form_cadastrar.php
+++ form_cadastrar.php
@@ -7,16 +7,38 @@
 <body>
     <pre>
         Cadastro de Materiais
-        <form action="inserir.php" method="POST">
+        <form id="form-adiciona" action="inserir.php" method="POST">
         Digite a quantidade <input type="number" name="quantidade" />
         Digite o comprimento (x) <input type="number" name="comprimento" />
         Digite a altura (y) <input type="number" name="altura" />
         Digite a chapa <input type="text" name="chapa" />
-        <input type="submit" value="Adicionar linha" />
-        <input type="submit" value="Remover linha" />
-        <input type="submit" value="Enviar" />
+        <input type="button" id="clonaLinha" value="Adicionar linha" />
+        <input type="button" id="removerLinha" value="Remover linha" />
+        <input type="submit" id="enviaDados" value="Enviar" />
         </form>
     </pre>
+
+    <script>
+        var clonarTabela = document.querySelector("#clonaLinha");
+
+        clonarTabela.addEventListener("click", function(event) {
+            event.target.parentNode.classList.add("#form-adiciona");
+
+        });
+    </script>
+    <script>
+        var removeTabela = document.querySelector("#removerLinha");
+
+        removeTabela.addEventListener("click", function(event) {
+            event.target.parentNode.classList.add("fadeOut");
+
+            setTimeout(function() {
+                event.target.parentNode.remove();
+            }, 500);
+
+        });
+    </script>
+
 </body>
 
 </html>
